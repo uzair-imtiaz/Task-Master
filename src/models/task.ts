9c29aec5c5e8fb3b59912f4e839e7aac5ad0c70b
Uzair Imtiaz <mtu880@gmail.com>
2024-12-14 17:22:08 +0500

    task model created

---
+++ src/models/task.ts
@@ -0,0 +1,40 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from 'config/database';
+import User from 'models/user';
+
+class Task extends Model {}
+
+Task.init(
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    status: {
+      type: DataTypes.ENUM('pending', 'completed'),
+      defaultValue: 'pending',
+    },
+    priority: {
+      type: DataTypes.ENUM('low', 'medium', 'high'),
+      defaultValue: 'low',
+    },
+    dueDate: {
+      type: DataTypes.DATE,
+    },
+    assignedTo: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: null,
+      references: {
+        model: User,
+        key: 'id',
+      },
+    },
+  },
+  { sequelize, modelName: 'Task', timestamps: true, tableName: 'tasks' }
+);
+
+export default Task;

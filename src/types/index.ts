5ec7eb799382f61ac052815da0cdf89a2d085e78
Uzair Imtiaz <mtu880@gmail.com>
2025-01-05 02:48:08 +0500

    add error middleware

---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+export { default as TaskAttributes } from './task';
+export { default as TaskCreationalAttributes } from './task';
+export { default as UserAttributes } from './user';
+export { default as UserCreationalAttributes } from './user';
